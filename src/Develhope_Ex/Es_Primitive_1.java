9b30ac18dd00dbfeda148336c98b8b8f303e31bb
Giovanni Battista Innaimi <giovanni.innaimi@protonmail.com>
2024-02-21 10:46:01 +0100

    Third Commit: Adding implicite casting

---
+++ src/Develhope_Ex/Es_Primitive_1.java
@@ -0,0 +1,10 @@
+package Develhope_Ex;
+
+public class Es_Primitive_1 {
+    public static void main(String[] args) {
+        char FirstChar = 'a';
+        char SecondChar ='b';
+
+        System.out.println(FirstChar + "" + SecondChar);
+    }
+}
