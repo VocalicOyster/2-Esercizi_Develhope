d4e7d80c1e32e3603676717055739ed141928027
Giovanni Battista Innaimi <giovanni.innaimi@protonmail.com>
2024-02-21 11:21:54 +0100

    Es: Function of sum of two integer (es: function 1)

---
+++ src/Develhope_Ex/EsFunction1.java
@@ -0,0 +1,14 @@
+package Develhope_Ex;
+
+public class EsFunction1 {
+    public static void main(String[] args) {
+        int numb1 = 10;
+        int numb2 = 20;
+        System.out.println(SumOf(numb1, numb2));
+    }
+
+
+    public static int SumOf(int FirstNumb, int SecondNumb) {
+        return FirstNumb + SecondNumb;
+    }
+}
