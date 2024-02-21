91d67eb6a610e58fee4e5e0eac8f2a82fe9e421c
Giovanni Battista Innaimi <giovanni.innaimi@protonmail.com>
2024-02-21 12:38:30 +0100

    Es: Comparison between two int (es: Comparison Operator 1)

---
+++ src/Develhope_Ex/EsCompOperator1.java
@@ -0,0 +1,27 @@
+package Develhope_Ex;
+
+public class EsCompOperator1 {
+    public static void main(String[] args) {
+        int num1 = 23;
+        int num2 = 22;
+
+        System.out.println("num 1: " + num1);
+        System.out.println("num 2: " + num2);
+
+        boolean result = num1 < num2;
+        System.out.println("num1 < num2: " + result);
+
+        result = num1 > num2;
+        System.out.println("num1 > num2: " + result);
+
+        result = num1 == num2;
+        System.out.println("num1 == num2: " + result);
+
+        result = num1 >= num2;
+        System.out.println("num1 >= num2: " + result);
+
+        result = num1 <= num2;
+        System.out.println("num1 <= num2: " + result);
+    }
+
+}
