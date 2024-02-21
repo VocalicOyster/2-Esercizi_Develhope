e21e676588f0215c13efcad3d635eba0f2073c48
Giovanni Battista Innaimi <giovanni.innaimi@protonmail.com>
2024-02-21 11:05:47 +0100

    Third Commit: Adding implicite casting

---
+++ src/Develhope_Ex/Es_Casting_1.java
@@ -0,0 +1,10 @@
+package Develhope_Ex;
+
+public class Es_Casting_1 {
+    public static void main(String[] args) {
+        double FirstNumb = 12d;
+        double SecondNumb = 29d;
+
+        System.out.println((int)FirstNumb + (int) SecondNumb);
+    }
+}
