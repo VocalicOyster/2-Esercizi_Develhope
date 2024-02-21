76a7dc37d16f9049ff20357bae4b2cde9be7e588
Giovanni Battista Innaimi <giovanni.innaimi@protonmail.com>
2024-02-21 11:47:29 +0100

    Es: Function division between three int (es: aritmetic operator 2)

---
+++ src/Develhope_Ex/EsAritmOperator2.java
@@ -0,0 +1,11 @@
+package Develhope_Ex;
+
+public class EsAritmOperator2 {
+    public static void main(String[] args) {
+        System.out.println(division3(12, 6, 2));
+    }
+
+    public static int division3(int numb1, int numb2, int numb3) {
+        return numb1 / numb2 / numb3;
+    }
+}
