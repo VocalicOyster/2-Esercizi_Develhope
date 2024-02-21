682bc7ae404dd78bf0e85f54c59e24d155329caa
Giovanni Battista Innaimi <giovanni.innaimi@protonmail.com>
2024-02-21 11:53:22 +0100

    Es: Function media between three double (es: aritmetic operator 3)

---
+++ src/Develhope_Ex/EsAritmOperator3.java
@@ -0,0 +1,12 @@
+package Develhope_Ex;
+
+public class EsAritmOperator3 {
+    public static void main(String[] args) {
+        System.out.println(media(10,10,15));
+    }
+
+    public static double media(double num1, double num2, double num3) {
+        double somma = num1 + num2 + num3;
+        return somma/3;
+    }
+}
