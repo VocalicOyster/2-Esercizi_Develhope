484960a99ffefc7c26912712fb27f0571d467d6c
Giovanni Battista Innaimi <giovanni.innaimi@protonmail.com>
2024-02-21 15:38:15 +0100

    Es: For cycle (es: For cycle 1)

---
+++ src/Develhope_Ex/EsforCycle1.java
@@ -0,0 +1,13 @@
+package Develhope_Ex;
+
+public class EsforCycle1 {
+    public static void main(String[] args) {
+        multWithFor(9);
+    }
+
+    public static void multWithFor(int numb) {
+        for(int i=0; i<=10; i++) {
+            System.out.println(numb * i);
+        }
+    }
+}
