ab0c4037913116a1d8b0f9d753a74fa20b58d5d7
Giovanni Battista Innaimi <giovanni.innaimi@protonmail.com>
2024-02-21 09:40:02 +0100

    First Commit

---
+++ src/Develhope_Ex/Es_Casting_2.java
@@ -0,0 +1,11 @@
+package Develhope_Ex;
+
+public class Es_Casting_2 {
+    public static void main(String[] args) {
+        char firstChar = 'f';
+        char secondChar = 'g';
+
+        int sum = (int)firstChar + (int)secondChar;
+        System.out.println(sum);
+    }
+}
