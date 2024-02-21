3d7afd6dbe0018a61fd4e1b2a1527380e87eb1ae
Giovanni Battista Innaimi <giovanni.innaimi@protonmail.com>
2024-02-21 09:43:06 +0100

    Second Commit: adding first exercise

---
+++ src/Develhope_Ex/Triangle_area.java
@@ -0,0 +1,12 @@
+package Develhope_Ex;
+
+public class Triangle_area {
+    public static void main(String[] args) {
+        float base = 2.5f;
+        int height = 3;
+
+        float area =  (base * height)/2;
+
+        System.out.print(area);
+    }
+}
