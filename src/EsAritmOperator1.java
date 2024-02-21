54a8feedd2d2d7089f4ae34beaf815765ee28ac1
Giovanni Battista Innaimi <giovanni.innaimi@protonmail.com>
2024-02-21 11:42:44 +0100

    Es: Function module between two int (es: aritmetic operator 1)

---
+++ src/EsAritmOperator1.java
@@ -0,0 +1,9 @@
+public class EsAritmOperator1 {
+    public static void main(String[] args) {
+        System.out.println(Mod(11, 6));
+    }
+
+    public static int Mod(int numb1, int numb2){
+        return numb1 % numb2;
+    }
+}
